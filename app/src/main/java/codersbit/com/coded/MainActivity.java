c8050cb38695b3cff75a7abf2a6b2df508a1e69a
Ofoegbu Ugochukwu <ugochukwu.ofoegbu.190737@unn.edu.ng>
2017-02-28 16:30:43 +0100

    Initial commit

---
+++ app/src/main/java/codersbit/com/coded/MainActivity.java
@@ -0,0 +1,15 @@
+package codersbit.com.coded;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = new TextView(this);
+    }
+}
